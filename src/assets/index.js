1d8118d5c47af7874b4019b32ab1b6bd2ad49fb1
Rodrigo Ferreira Schaer <rodrigo.fscs@gmail.com>
2024-07-22 12:49:19 -0300

    initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+import studyPlanDetail from "./study-plan-detail.json" assert { type: "json" };
+import studyPlanProgress from "./study-plan-progress.json" assert { type: "json" };
+
+export { studyPlanDetail, studyPlanProgress };
