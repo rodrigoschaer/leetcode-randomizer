af7fab76ac9bfc2a1594a1b299201b03cea47f27
Rodrigo Ferreira Schaer <rodrigo.fscs@gmail.com>
2024-07-24 11:34:40 -0300

    feat: creates Randomizer class

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import studyPlanDetail from "./study-plan-detail.json";
-import studyPlanProgress from "./study-plan-progress.json";
+import studyPlanDetail from './study-plan-detail.json';
+import studyPlanProgress from './study-plan-progress.json';
 
 export { studyPlanDetail, studyPlanProgress };
