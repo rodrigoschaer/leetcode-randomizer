b65e169a709463e7e255da37c150bf7f583c14ac
Rodrigo Ferreira Schaer <rodrigo.fscs@gmail.com>
2024-07-25 19:01:37 -0300

    chore: prettier config and formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,11 @@
-import { fixupConfigRules } from "@eslint/compat";
-import globals from "globals";
-import babelParser from "@babel/eslint-parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+/* eslint-disable import/no-extraneous-dependencies */
+import { fixupConfigRules } from '@eslint/compat';
+import globals from 'globals';
+import babelParser from '@babel/eslint-parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -17,34 +18,37 @@ const compat = new FlatCompat({
 export default [
   ...fixupConfigRules(
     compat.extends(
-      "eslint:recommended",
-      "plugin:import/errors",
-      "plugin:import/warnings",
-      "airbnb-base"
-    )
+      'eslint:recommended',
+      'plugin:import/errors',
+      'plugin:import/warnings',
+      'airbnb-base',
+      'prettier',
+    ),
   ),
   {
     languageOptions: {
       globals: {
-        ...globals.browser,
         ...globals.node,
       },
 
       parser: babelParser,
       ecmaVersion: 12,
-      sourceType: "module",
+      sourceType: 'module',
 
       parserOptions: {
         requireConfigFile: false,
 
         babelOptions: {
-          presets: ["@babel/preset-env"],
+          presets: ['@babel/preset-env'],
         },
       },
     },
-
     rules: {
-      // personal rules
+      'no-console': 'off',
+      'class-methods-use-this': 'off',
+      'import/prefer-default-export': 'off',
+      'no-underscore-dangle': 'off',
+      'import/extensions': 'off',
     },
   },
 ];
